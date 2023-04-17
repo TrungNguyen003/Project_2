a47dd7ac6534a032e94c38d45c7e763c72b64738
TrungNguyen003 <trungnt.114010121040@vtc.edu.vn>
2023-04-18 01:19:18 +0700

    dau vao

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<header class="header">
+        <a href="" class="logo">Pet.</a>
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+        <ul class="menu">
+            <li><a href="#work">về chúng tôi</a></li>
+            <li><a href="#contact">Mới</a></li>
+            <li><a href="#contact">new</a></li>
+            <div class="rightt">
+                <li id="item1"><a href="#contact">Đăng nhập</a></li>
+                <li id="item2"><a href="#contact">Đăng kí</a></li>
+            </div>     
+        </ul>
+    </header>
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/style_index.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <title>🐶</title>
+</head>
+
+<body>
+    <?php include('includes/header.php'); ?>
+    <main>&nbsp</main>
+        <div class="content-left">
+            <h1>
+                Open <br> the menu to <br> see more pets
+            </h1>
+            <p> Một chú chó không nhất thiết phải là cả cuộc đời bạn nhưng <br>chúng có thể khiến cuộc sống của bạn thêm màu sắc và toàn diện hơn.</p>
+        </div>
+        <div class="content-min"></div>
+        <div class="content-right">
+            <img id="anh1" src="assets/img/unnamed.jpg">
+            <img id="anh2" src="assets/img/341751645_3419254721632567_2835827517458195052_n.png">
+        </div>
+    <div class="button-move">
+            <button class="btn btn-info">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <h3>Shop &nbsp <i class="fa-solid fa-arrow-right"></i></h3>
+            </button>
+    </div>
+</body>
+
+</html>
