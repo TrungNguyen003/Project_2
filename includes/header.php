ff1f9bfacb9e124678314f6714e2819d6c4b4e95
Trung Nguyen <trungnt.114010121040@vtc.edu.vn>
2023-04-27 17:17:02 +0700

    trang user

--- includes/header.php
+++ includes/header.php
@@ -1,46 +1,44 @@
     <?php if ($_SESSION['login']) {
     ?>
-        <section class="menu-section">
-            <div class="container">
-                <div class="row ">
-                    <div class="col-md-12">
-                        <div class="navbar-collapse collapse ">
-                            <ul id="menu-top" class="nav navbar-nav navbar-right">
-                                <li><a href="bangdieukhien.php" class="menu-top-active">Bảng điều khiển</a></li>
-                                <li><a href="muonsach.php" class="menu-top-active"> Mượn sách</a></li>
-                                <li>
-                                    <a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Quản lý sách của tôi <i class="fa-solid fa-id-card"></i> &nbsp;<i class="fa fa-angle-down"></i></a>
-                                    <ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem">
-                                        <li role="presentation"><a role="menuitem" tabindex="-1" href="sachdaphathanh.php"> Sách đã mượn </a></li>
-                                        <li role="presentation"><a role="menuitem" tabindex="-1" href=""> sách đã mượn online</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown"> Tài khoản <i class="fa-solid fa-id-card"></i> &nbsp;<i class="fa fa-angle-down"></i></a>
-                                    <ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem">
-                                        <li role="presentation"><a role="menuitem" tabindex="-1" href="taikhoannguoidung.php">Tài khoảng của tôi </a></li>
-                                        <li role="presentation"><a role="menuitem" tabindex="-1" href="">Đổi mật khẩu</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <header class="header" id="navbar">
+            <a href="" class="logo">Pet.</a>
+            <nav>
+  <label for="drop" class="toggle">&#8801; Menu</label>
+  <input type="checkbox" id="drop" />
+  <ul class="menu">
+    <li><a href="#">Home</a></li>
+    <li> 
+      <!-- First Tier Drop Down -->
+      <label for="drop-1" class="toggle">Service +</label>
+      <a href="#">Service</a>
+      <input type="checkbox" id="drop-1"/>
+      <ul>
+        <li><a href="#">Service 1</a></li>
+        <li><a href="#">Service 2</a></li>
+        <li><a href="#">Service 3</a></li>
+      </ul>
+    </li>
+    <li> <a href="#">Portfolio</a></li>
+    <li><a href="#">Blog</a></li>
+    <li><a href="#">Submit</a></li>
+    <li><a href="#">Contact</a></li>
+    <li><a href="#">About</a></li>
+  </ul>
+</nav>
+        </header>
     <?php } else { ?>
-        <header class="header">
-        <a href="" class="logo">Pet.</a>
-        <input class="menu-btn" type="checkbox" id="menu-btn" />
-        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-        <ul class="menu">
-            <li><a href="#work">về chúng tôi</a></li>
-            <li><a href="#contact">Mới</a></li>
-            <li><a href="#contact">new</a></li>
-            <div class="rightt">
-                <li id="item1"><a href="#contact">Đăng nhập</a></li>
-                <li id="item2"><a href="#contact">Đăng kí</a></li>
-            </div>     
-        </ul>
-    </header>
+        <header class="header" id="navbar">
+            <a href="" class="logo">Pet.</a>
+            <input class="menu-btn" type="checkbox" id="menu-btn" />
+            <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+            <ul class="menu">
+                <li><a href="#work">về chúng tôi</a></li>
+                <li><a href="#contact">Mới</a></li>
+                <li><a href="#contact">new</a></li>
+                <div class="rightt">
+                    <li id="item1"><a href="#contact">Đăng nhập</a></li>
+                    <li id="item2"><a href="#contact">Đăng kí</a></li>
+                </div>
+            </ul>
+        </header>
     <?php } ?>
--- mainshop.php
+++ mainshop.php
@@ -14,86 +14,54 @@ if (strlen($_SESSION['login']) == 0) {
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
     <link href="bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
-    <link href="style.css" rel="stylesheet" />
+    <link href="assets/css/style_main.css" rel="stylesheet" />
     <title>Document</title>
 </head>
 
 <body>
     <?php include('includes/header.php'); ?>
-    <div class="content-wrapper">
-        <div class="container">
-            <div class="row pad-botm">
-                <div class="col-md-12">
-                    <h4 class="header-line">BẢNG ĐIỀU KHIỂN NGƯỜI DÙNG</h4>
-                </div>
-            </div>
-            <div class="row">
-                <a href="lietkesach.php">
-                    <div class="col-md-4 col-sm-4 col-xs-6">
-                        <div class="alert alert-success back-widget-set text-center">
-                            <i class="fa fa-book fa-5x"></i>
-                            <?php
-                            $sql = "SELECT id from sach ";
-                            $query = $dbh->prepare($sql);
-                            $query->execute();
-                            $results = $query->fetchAll(PDO::FETCH_OBJ);
-                            $listdbooks = $query->rowCount();
-                            ?>
-                            <h3><?php echo htmlentities($listdbooks); ?></h3>Thư Viện Sách
-                        </div>
-                    </div>
-                </a>
-                <div class="col-md-4 col-sm-4 col-xs-6">
-                    <div class="alert alert-warning back-widget-set text-center">
-                        <i class="fa-sharp fa-solid fa-arrows-rotate fa-5x"></i>
-                        <?php
-                        $rsts = 0;
-                        $sid = $_SESSION['id'];
-                        $sql2 = "SELECT id from chitietsachdaphathanh where IDNguoiDung=:sid and (RetrunTrangThai=:rsts || RetrunTrangThai is null || RetrunTrangThai='')";
-                        $query2 = $dbh->prepare($sql2);
-                        $query2->bindParam(':sid', $sid, PDO::PARAM_STR);
-                        $query2->bindParam(':rsts', $rsts, PDO::PARAM_STR);
-                        $query2->execute();
-                        $results2 = $query2->fetchAll(PDO::FETCH_OBJ);
-                        $returnedbooks = $query2->rowCount();
-                        ?>
-                        <h3><?php echo htmlentities($returnedbooks); ?></h3>Sách Chưa Trả
-                    </div>
-                </div>
-                <a href="sachdamuon.php">
-                <div class="col-md-4 col-sm-4 col-xs-6">
-                    <div class="alert alert-danger back-widget-set text-center">
-                        <i class="fa-solid fa-exclamation fa-5x"></i>
-                        <?php
-                                $rsts = 0;
-                                $sid = $_SESSION['id'];
-                                $sql3 = "SELECT id from duyetmuonsach where idnguoidung=:sid and (trangthaimuon=:rsts || trangthaimuon is null || trangthaimuon='')";
-                                $query3 = $dbh->prepare($sql3);
-                                $query3->bindParam(':sid', $sid, PDO::PARAM_STR);
-                                $query3->bindParam(':rsts', $rsts, PDO::PARAM_STR);
-                                $query3->execute();
-                                $results3 = $query3->fetchAll(PDO::FETCH_OBJ);
-                                $trangthai = $query3->rowCount();
-                                ?>
-                                <h3><?php echo htmlentities($trangthai); ?> </h3>
-                                Sách online chưa trả/ Đang yêu cầu mượn
-                    </div>
-                </div>
-                </a>
-                <a href="sachdaphathanh.php">
-                    <div class="col-md-4 col-sm-4 col-xs-6">
-                        <div class="alert alert-success back-widget-set text-center">
-                        <i class="fa-solid fa-book-open-reader fa-5x"></i>
-                            <h3>&nbsp;</h3>Sách đang được mượn
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </div>
-    </div>
-    <?php include('footer.php'); ?>
-    <script src="jquery-1.10.2.js"></script>
-    <script src="bootstrap.js"></script>
+    <div class="content">
+  <h3>Sticky Navigation Example</h3>
+  <p>The header will stick to the top when you reach its scroll position.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+</div><div class="content">
+  <h3>Sticky Navigation Example</h3>
+  <p>The header will stick to the top when you reach its scroll position.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+</div><div class="content">
+  <h3>Sticky Navigation Example</h3>
+  <p>The header will stick to the top when you reach its scroll position.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+</div><div class="content">
+  <h3>Sticky Navigation Example</h3>
+  <p>The header will stick to the top when you reach its scroll position.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+</div>
+    <script src="assets/js.js"></script>
 </body>
 </html>
 <?php } ?>
