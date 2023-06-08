c6a24cddb4fc1d42713378fe8ed4a66240cf2d4a
TrungNguyen003 <trungnt.114010121040@vtc.edu.vn>
2023-06-08 18:46:57 +0700

    ...

--- dangxuat.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_destroy(); // lệnh hủy tất cả các biến session
-header("location:index.php"); 
-?>
--- model/dichvu_list.php
+++
@@ -1,6 +0,0 @@
-<?php
- $sql = "SELECT sanpham.TenSP,theloai.TenTheLoai,phanloai.TenPhanLoai,sanpham.MaSP,sanpham.GiaSP,sanpham.id as idsp,sanpham.HinhSP,sanpham.DuocPhatHanh from  sanpham join theloai on theloai.id=sanpham.CatId join phanloai on phanloai.id=sanpham.IDPhanLoai WHERE theloai.TenTheLoai ='Dịch vụ' ";
- $query = $dbh->prepare($sql);
- $query->execute();
- $results = $query->fetchAll(PDO::FETCH_OBJ);
-?>
